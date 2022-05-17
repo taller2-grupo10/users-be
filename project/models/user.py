103ac891af76c9d106016e9020d714715427a6e9
francisco <fbertolotto@fi.uba.ar>
2022-05-17 17:28:44 -0300

    [migration] adds artist_id to sql db

--- project/models/user.py
+++ project/models/user.py
@@ -8,6 +8,7 @@ class User(BaseModel):
 
     uid = db.Column(db.String(128), unique=True, nullable=False)
     active = db.Column(db.Boolean(), default=True, nullable=False)
+    artist_id = db.Column(db.String(128), nullable=True)
 
     users_roles = db.relationship("UserRole", back_populates="user", lazy="joined")
 
@@ -25,6 +26,7 @@ class User(BaseModel):
     def __repr__(self):
         return f"<User {self.uid}>"
 
-    def __init__(self, uid):
+    def __init__(self, uid, artist_id):
         self.uid = uid
         self.active = True
+        self.artist_id = artist_id
