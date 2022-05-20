c9875cbe8f89b82173f07ccb1ba21a5cc19436bb
Mauro Santoni <mausantoni@gmail.com>
2022-05-20 20:08:50 -0300

    Add example response for genres get

--- project/blueprints/media_genres_blueprint.py
+++ project/blueprints/media_genres_blueprint.py
@@ -8,6 +8,34 @@ api = Namespace(
     description="Music Genres related endpoints",
 )
 
+music_genres_response_model = fields.List(
+    fields.String(
+        required=False,
+        description="Music Genres",
+    ),
+    example=[
+        "Alternative",
+        "Blues",
+        "Classical",
+        "Country",
+        "Dance",
+        "Electronic",
+        "Folk",
+        "Funk",
+        "Hip-Hop",
+        "Heavy Metal",
+        "Instrumental",
+        "Jazz",
+        "Pop",
+        "R&B",
+        "Reggae",
+        "Rock",
+        "Soul",
+        "Trap",
+        "Other",
+    ],
+)
+
 
 # ----------------------------------------------------------------------
 # Routes
@@ -16,7 +44,7 @@ api = Namespace(
 @api.route("")
 class MusicGenres(Resource):
     # @check_token
-    @api.response(200, "Success", [])
+    @api.response(200, "Success", music_genres_response_model)
     def get(self):
         response, status_code = MediaRequester.get(f"genres")
         return response, status_code
