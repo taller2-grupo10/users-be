2b171cb10e20502480e945094df9d17bc8ba84f5
Mauro Santoni <mausantoni@gmail.com>
2022-07-03 18:15:28 -0300

    add album media admin endpoints

--- project/blueprints/admin_media_blueprint.py
+++ project/blueprints/admin_media_blueprint.py
@@ -6,6 +6,7 @@ from flask import jsonify
 from flask_restx import Namespace, Resource, fields
 from project.blueprints.media_song_blueprint import song_response_model
 from project.blueprints.media_playlist_blueprint import playlist_response_model
+from project.blueprints.media_album_blueprint import album_response_model
 
 api = Namespace(
     name="Admin Media", path="admin/media", description="Admin Media related endpoints"
@@ -77,5 +78,40 @@ class AdminPlaylistDisable(Resource):
     def post(self, id):
         response, status_code = MediaRequester.put(
             f"playlists/{id}", data={"isActive": False}
+        )
+        return response, status_code
+
+
+# Albums
+@api.route("/albums")
+class AdminAlbum(Resource):
+    @check_token
+    @check_permissions(["admin_list"])
+    @api.response(200, "Success", album_response_model)
+    def get(self):
+        response, status_code = MediaRequester.get("albums/noFilter")
+        return response, status_code
+
+
+@api.route("/albums/enable/<id>")
+class AdminAlbumEnable(Resource):
+    @check_token
+    @check_permissions(["admin_modify"])
+    @api.response(200, "Success", album_response_model)
+    def post(self, id):
+        response, status_code = MediaRequester.put(
+            f"albums/{id}", data={"isActive": True}
+        )
+        return response, status_code
+
+
+@api.route("/albums/disable/<id>")
+class AdminAlbumDisable(Resource):
+    @check_token
+    @check_permissions(["admin_modify"])
+    @api.response(200, "Success", album_response_model)
+    def post(self, id):
+        response, status_code = MediaRequester.put(
+            f"albums/{id}", data={"isActive": False}
         )
         return response, status_code
